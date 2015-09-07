a03df28c15578b3b97c182026bcf892862c1290b
unknown <samarthagarwal@live.com>
2015-09-07 16:36:05 +0530

    CPP

---
+++ .bash_history
@@ -0,0 +1,13 @@
+g++ -o main *.cpp
+main
+main
+g++ -o main *.cpp
+g++ -o main *.cpp
+g++ -o main *.cpp
+g++ -o main exercise3.cpp
+main
+g++ -o main exercise3.cpp
+main
+main
+g++ -o main exercise3.cpp
+main
---
+++ exercise2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 7;
+    int y = 3;
+    float a = 7.0;
+    float b = 3.0;
+
+    cout << "7/3 = " << x/y << endl;
+    cout << "7.0/3 = " << a/y << endl;
+    cout << "7.0/3.0 = " << a/b << endl;
+    cout << "7%3 = " << x%y << endl;
+
+}
